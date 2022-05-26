a4d138950411092c0bfd5ef4d7b8d0d8b3606afc
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2022-05-26 21:53:48 +0700

    update system apps: created project starter

--- app/src/main/java/com/gunder/academy/ui/home/HomeActivity.kt
+++ app/src/main/java/com/gunder/academy/ui/home/HomeActivity.kt
@@ -1,7 +1,8 @@
-package com.gunder.academy
+package com.gunder.academy.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gunder.academy.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
