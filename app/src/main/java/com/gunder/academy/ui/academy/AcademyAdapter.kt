5b455e1c5eb88a76cc6d75ee3e76b2d1e8a0c7bf
gunadermawan <gunadermawan62@gmail.com>
2022-06-02 08:12:44 +0700

    update system: created AcademyAdapter.kt

---
+++ app/src/main/java/com/gunder/academy/ui/academy/AcademyAdapter.kt
@@ -0,0 +1,50 @@
+package com.gunder.academy.ui.academy
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.gunder.academy.R
+import com.gunder.academy.data.CourseEntity
+import com.gunder.academy.databinding.ItemsAcademyBinding
+import com.gunder.academy.ui.detail.DetailCourseActivity
+
+class AcademyAdapter : RecyclerView.Adapter<AcademyAdapter.CourseViewHolder>() {
+    private var listCourses = ArrayList<CourseEntity>()
+
+    class CourseViewHolder(private val binding: ItemsAcademyBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(course: CourseEntity) {
+            with(binding) {
+                tvItemTitle.text = course.title
+                tvItemDate.text =
+                    itemView.resources.getString(R.string.deadline_date, course.deadline)
+                itemView.setOnClickListener {
+                    val intent = Intent(itemView.context, DetailCourseActivity::class.java)
+                    intent.putExtra(DetailCourseActivity.EXTRA_COURSE, course.courseId)
+                    itemView.context.startActivity(intent)
+                }
+                Glide.with(itemView.context)
+                    .load(course.imagePath)
+                    .apply(RequestOptions.placeholderOf(R.drawable.ic_loading))
+                    .error(R.drawable.ic_error)
+                    .into(ivPoster)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CourseViewHolder {
+        val itemAcademyBinding =
+            ItemsAcademyBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CourseViewHolder(itemAcademyBinding)
+    }
+
+    override fun onBindViewHolder(holder: CourseViewHolder, position: Int) {
+        val course = listCourses[position]
+        holder.bind(course)
+    }
+
+    override fun getItemCount(): Int = listCourses.size
+}
