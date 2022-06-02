087ff8fcd9435fd7cb0bc658fa5f2f5682af074d
gunadermawan <gunadermawan62@gmail.com>
2022-06-02 07:49:02 +0700

    update UI : Layouting

--- app/src/main/java/com/gunder/academy/ui/detail/DetailCourseActivity.kt
+++ app/src/main/java/com/gunder/academy/ui/detail/DetailCourseActivity.kt
@@ -6,32 +6,29 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.google.android.material.snackbar.Snackbar
 import com.gunder.academy.R
 import com.gunder.academy.databinding.ActivityDetailCourseBinding
+import com.gunder.academy.databinding.ContentDetailCourseBinding
 
 class DetailCourseActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityDetailCourseBinding
+    private lateinit var activityDetailCourseBinding: ActivityDetailCourseBinding
+    private lateinit var detailContentBinding: ContentDetailCourseBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityDetailCourseBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        activityDetailCourseBinding = ActivityDetailCourseBinding.inflate(layoutInflater)
+        detailContentBinding = activityDetailCourseBinding.detailContent
+        setContentView(activityDetailCourseBinding.root)
 
-        setSupportActionBar(binding.toolbar)
+        setSupportActionBar(activityDetailCourseBinding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val navController = findNavController(R.id.nav_host_fragment_content_detail_course)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
