d253d3168e07c4b1940bc2cb035e4b2b26ea059a
gunadermawan <gunadermawan62@gmail.com>
2022-06-11 06:04:08 +0700

    update system: make a connections beetwen HomeActivity.kt and SectionsPagerAdapter.kt and also create CourseReaderCallback.kt

--- app/src/main/java/com/gunder/academy/ui/home/SectionsPagerAdapter.kt
+++ app/src/main/java/com/gunder/academy/ui/home/SectionsPagerAdapter.kt
@@ -1,4 +1,30 @@
 package com.gunder.academy.ui.home
 
-class SectionsPagerAdapter {
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.gunder.academy.R
+import com.gunder.academy.ui.academy.Academy
+import com.gunder.academy.ui.bookmark.Bookmark
+
+class SectionsPagerAdapter(private val mContext: Context, fm: FragmentManager) :
+    FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    override fun getCount(): Int = 2
+
+    override fun getPageTitle(position: Int): CharSequence? =
+        mContext.resources.getString(TAB_TITLES[position])
+
+    override fun getItem(position: Int): Fragment =
+        when (position) {
+            0 -> Academy()
+            1 -> Bookmark()
+            else -> Fragment()
+        }
+
+
+    companion object {
+        private val TAB_TITLES = intArrayOf(R.string.home, R.string.bookmark)
+    }
+
 }
